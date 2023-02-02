4673552de1695d6ea1d8337b2f40f54b24fd4031
Héctor Gonzalo Andrés <hectorgoan@Hectors-MacBook-Pro.local>
2023-02-02 19:34:38 +0100

    Good state. Need to work on opening links when clicked

---
+++ GMailMac/GMailMac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMailMac
+//
+//  Created by Héctor Gonzalo Andrés on 02/02/2023.
+//
+
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    let url = "https://mail.google.com/mail/"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let request = URLRequest(url: URL(string: url)!)
+        webView.load(request)
+    }
+}
+
